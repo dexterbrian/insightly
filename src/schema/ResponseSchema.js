da8a068335a2828cf88582af61f4539151dbf673
Brian Waweru <brianwaweru627@gmail.com>
2024-09-22 17:01:26 +0300

    Created ResponseSchema

---
+++ src/schema/ResponseSchema.js
@@ -0,0 +1,32 @@
+import { mongoose } from "mongoose";
+
+const { Schema } = mongoose;
+
+const responseSchema = new Schema({
+    question_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    questionnaire_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    choice_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    respondent: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default responseSchema;
