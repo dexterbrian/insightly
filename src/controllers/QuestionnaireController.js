39ac74da3681aac22526a7de6942e6185247e3b3
Brian Waweru <brianwaweru627@gmail.com>
2024-09-22 20:08:32 +0300

    Enabled getting a questionnaire

--- src/controllers/QuestionnaireController.js
+++ src/controllers/QuestionnaireController.js
@@ -1,21 +1,34 @@
-import { insertQuestionnaire } from "../models/QuestionnairesModel.js";
-import { insertQuestions } from "../models/QuestionsModel.js";
+import Questionnaire from "../models/QuestionnairesModel.js";
+import Question from "../models/QuestionsModel.js";
 
 export const createQuestionnaire = async (req, res) => {
   try {
     const { creator, title, questions } = req.body;
-    let newQuestionnaire = await insertQuestionnaire({ creator, title });
+    let newQuestionnaire = await Questionnaire.create({ creator, title });
     console.log('questionnaire: ', newQuestionnaire);
 
     const newQuestions = questions.map(question => ({ questionnaire_id: newQuestionnaire._id, ...question }));
-    const insertedQuestions = await insertQuestions(newQuestions);
+    const insertedQuestions = await Question.insertMany(newQuestions);
     console.log(insertedQuestions);
 
-    res.status(201).json({ newQuestionnaire, questions: insertedQuestions });
+    res.status(201).json({ questionnaire: newQuestionnaire, questions: insertedQuestions });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'Error creating questionnaire' });
   }
 };
 
-export default createQuestionnaire;
+export const getQuestionnaire = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const questionnaire = await Questionnaire.findById(id);
+
+    // Get the questions for this specific questionnaire
+    const questions = await Question.find({ questionnaire_id: id });
+
+    res.status(200).json({ questionnaire, questions });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error getting questionnaire' });
+  }
+};
