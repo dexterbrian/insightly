00db6e20439a48e7196493753563a4f807d1b7aa
Brian Waweru <brianwaweru627@gmail.com>
2024-09-22 12:49:15 +0300

    updated the mongodb connection link

--- server.js
+++ server.js
@@ -2,28 +2,29 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const routes = require('./src/routes');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 // Change the server for a local or a cluster of your own
 const server =
-  'mongodb+srv://candidate:interview@interview.usfbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+  `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(process.env.DB_PASSWORD)}@insightlycluster0.jupd9.mongodb.net/insightly?retryWrites=true&w=majority&appName=InsightlyCluster0`;
 
 app.use('/', routes);
 
-mongoose
-  .connect(server, {
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`API listening on http://localhost:${port}`);
+});
+
+mongoose.connect(server, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log('Database connected');
-    const port = process.env.PORT || 3000;
-
-    app.listen(port, () => {
-      console.log(`API listening on http://localhost:${port}`);
-    });
   })
   .catch((err) => console.log(err));
 
