f203ca3dcfe3f96879fc25bb9625e7ec60271f40
Brian Waweru <brianwaweru627@gmail.com>
2024-09-22 16:59:11 +0300

    Created route POST /api/questionnaires to create questionnaires

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,26 @@
-const express = require('express');
+import express from 'express';
+import Example from './modelExample.js';
+import { createQuestionnaire } from './controllers/QuestionnaireController.js';
 
-const Example = require('./modelExample');
-
-const router = new express.Router();
+const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+  res.sendFile(__dirname + '/index.html');
+});
 
-//Example post
+// Example post
 router.post('/example', async (req, res) => {
-    const exampleData = req.body.example;
+  const exampleData = req.body.example;
 
-    await Example.create({ attribute1: exampleData })
-        .then(result => {
-            res.status(201).json({ Result: result.attribute1 })
-            return result;
-        })
-        .catch(error => {console.log(error)})
-})
+  await Example.create({ attribute1: exampleData })
+    .then(result => {
+        res.status(201).json({ Result: result.attribute1 })
+        return result;
+    })
+    .catch(error => {console.log(error)})
+});
 
-module.exports = router ;
+// Create Questionnaire
+router.post('/api/questionnaires', (req, res) => createQuestionnaire(req, res));
+
+export default router;
