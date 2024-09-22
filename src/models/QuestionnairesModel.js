ff78eb93f3d4eafefc80787a96058c23ad1c3b8e
Brian Waweru <brianwaweru627@gmail.com>
2024-09-22 20:09:14 +0300

    Removed unnecessary code in QuestionnairesModel and QuestionsModel

--- src/models/QuestionnairesModel.js
+++ src/models/QuestionnairesModel.js
@@ -4,13 +4,4 @@ import questionnaireSchema from "../schema/QuestionnaireSchema.js";
 
 const Questionnaire = model('Questionnaire', questionnaireSchema);
 
-/**
- * Creates a new questionnaire in the database.
- *
- * @async
- * @param {{ creator: string, title: string }} questionnaireData - The data to create the questionnaire with.
- * @returns {Promise<{ _id: string, creator: string, title: string, createdAt: Date, updatedAt: Date }>} The newly created questionnaire.
- */
-export const insertQuestionnaire = async (questionnaireData) => await Questionnaire.create(questionnaireData);
-
 export default Questionnaire;
--- src/models/QuestionsModel.js
+++ src/models/QuestionsModel.js
@@ -4,8 +4,4 @@ import questionSchema from "../schema/QuestionSchema.js";
 
 const Question = model('Question', questionSchema);
 
-export const insertQuestions = async (questionData) => {
-    return await Question.insertMany(questionData);
-};
-
 export default Question;
