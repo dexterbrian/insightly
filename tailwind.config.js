afcd4179c4a39c5d955f3183dfcae1b32bf9a133
Brian Waweru <brianwaweru627@gmail.com>
2024-09-23 02:23:58 +0300

    Installed tailwind, autoprefixer and postcss cli

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: [
+    './src/views/*.html', // Update this path to match your HTML file paths
+  ],
+  darkMode: false, // or 'media' or 'class'
+  variants: {},
+}
+
