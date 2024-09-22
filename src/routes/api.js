2d59350d51e33fd95f663e62e1b36e6bda136813
Brian Waweru <brianwaweru627@gmail.com>
2024-09-22 20:48:08 +0300

    Added route POST /questionnaires/:id/submit for submitting responses to questions in a questionnaire

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createQuestionnaire, getQuestionnaire } from '../controllers/QuestionnaireController.js';
+import { createQuestionnaire, getQuestionnaire, submitQuestionnaireResponses } from '../controllers/QuestionnaireController.js';
 
 const apiRouter = express.Router();
 
@@ -8,5 +8,8 @@ apiRouter.post('/questionnaires', (req, res) => createQuestionnaire(req, res));
 
 // Get Questionnaire
 apiRouter.get('/questionnaires/:id', (req, res) => getQuestionnaire(req, res));
+
+// Submit Responses to a Questionnaire
+apiRouter.post('/questionnaires/:id/submit', (req, res) => submitQuestionnaireResponses(req, res));
 
 export default apiRouter;
